df7822b11f873d73d618e3c3443539b33ae958db
Skeez Laps <zzz05658@gmail.com>
2019-11-07 12:52:09 +0200

    MVS base components

--- js/main.js
+++ js/main.js
@@ -11,3 +11,113 @@ class EventEmitter {
         (this._events[evt] || []).slice().forEach(lsn => lsn(arg));
     }
 }
+// Model
+class ListModel extends EventEmitter {
+    constructor(items) {
+        super();
+        this._items = items || [];
+        this._selectedIndex = -1;
+    }
+
+    getItems() {
+        return this._items.slice();
+    }
+
+    addItem(item) {
+        this._items.push(item);
+        this.emit('itemAdded', item);
+    }
+
+    removeItemAt(index) {
+        const item = this._items.splice(index, 1)[0];
+        this.emit('itemRemoved', item);
+        if (index === this._selectedIndex) {
+            this.selectedIndex = -1;
+        }
+    }
+
+    get selectedIndex () {
+        return this._selectedIndex;
+    }
+
+    set selectedIndex(index) {
+        const previousIndex = this._selectedIndex;
+        this._selectedIndex = index;
+        this.emit('selectedIndexChanged', previousIndex);
+    }
+}
+//  View
+class ListView extends EventEmitter {
+    constructor(model, elements) {
+        super();
+        this._model = model;
+        this._elements = elements;
+
+        // attach model listeners
+        model.on('itemAdded', () => this.rebuildList())
+            .on('itemRemoved', () => this.rebuildList());
+
+        // attach listeners to HTML controls
+        elements.list.addEventListener('change',
+            e => this.emit('listModified', e.target.selectedIndex));
+        elements.addButton.addEventListener('click',
+            () => this.emit('addButtonClicked'));
+        elements.delButton.addEventListener('click',
+            () => this.emit('delButtonClicked'));
+    }
+
+    show() {
+        this.rebuildList();
+    }
+
+    rebuildList() {
+        const list = this._elements.list;
+        list.options.length = 0;
+        this._model.getItems().forEach(
+            item => list.options.add(new Option(item)));
+        this._model.selectedIndex = -1;
+    }
+}
+
+// Controller
+class ListController {
+    constructor(model, view) {
+        this._model = model;
+        this._view = view;
+
+        view.on('listModified', idx => this.updateSelected(idx));
+        view.on('addButtonClicked', () => this.addItem());
+        view.on('delButtonClicked', () => this.delItem());
+    }
+
+    addItem() {
+        const item = window.prompt('Add item:', '');
+        if (item) {
+            this._model.addItem(item);
+        }
+    }
+
+    delItem() {
+        const index = this._model.selectedIndex;
+        if (index !== -1) {
+            this._model.removeItemAt(index);
+        }
+    }
+
+    updateSelected(index) {
+        this._model.selectedIndex = index;
+    }
+}
+
+window.addEventListener('load', () => {
+    const model = new ListModel(['node.js', 'react']),
+        view = new ListView(model, {
+            'list' : document.getElementById('list'),
+            'addButton' : document.getElementById('plusBtn'),
+            'delButton' : document.getElementById('minusBtn')
+        }),
+        controller = new ListController(model, view);
+
+    view.show();
+});
+
